64300a61b234af24993fb0d776d95238f2c49374
Krustycoin0 <133280489+Krustycoin0@users.noreply.github.com>
2023-05-17 13:57:59 +0200

    Update Bridgeswap.tsx

--- src/bridges/Bridgeswap.tsx
+++ src/bridges/Bridgeswap.tsx
@@ -4,6 +4,7 @@ const widgetConfig: WidgetConfig = {
   integrator: 'Lisprocoin',
   fromChain: 56,
   fromToken: '0xE62A9bc6eDe534E18Dd2793Dcaf5A2B6df112180',
+  logoToken:'https://i.postimg.cc/T3DWSPJ5/IMG-20230509-182205.jpg',
   containerStyle: {
     border: '1px solid rgb(234, 234, 234)',
     borderRadius: '16px',
